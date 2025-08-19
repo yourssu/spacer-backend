42132c09804d14961740d6e2a0bd7b1be4b7e1e5
yueunfive <122276414+yueunfive@users.noreply.github.com>
2025-08-19 14:39:58 +0900

    feat: initial project setup

---
+++ src/main/kotlin/com/yourssu/spacer/SpacerApplication.kt
@@ -0,0 +1,11 @@
+package com.yourssu.spacer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpacerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpacerApplication>(*args)
+}
---
+++ src/test/kotlin/com/yourssu/spacer/SpacerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yourssu.spacer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpacerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
