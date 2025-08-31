b2c982a247d1118f92b8f92bff46434ba8492075
yueunfive <122276414+yueunfive@users.noreply.github.com>
2025-08-31 19:47:42 +0900

    [#4]; test: contextLoads 테스트 내 DiscordBot MockBean 추가

--- src/test/kotlin/com/yourssu/spacer/SpacerApplicationTests.kt
+++ src/test/kotlin/com/yourssu/spacer/SpacerApplicationTests.kt
@@ -1,15 +1,19 @@
 package com.yourssu.spacer
 
+import com.yourssu.spacer.spacehub.application.domain.discord.DiscordBot
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
 @ActiveProfiles("test")
 class SpacerApplicationTests {
 
+    @MockBean
+    private lateinit var discordBot: DiscordBot
+
     @Test
     fun contextLoads() {
     }
-
 }
